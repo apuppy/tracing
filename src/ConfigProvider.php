e47dd548f945d6e310c7ffb5a460e487cbaabd31
lihongde <chinalihongde@gmail.com>
2021-11-02 16:36:01 +0800

    ConfigProvider

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -9,16 +9,18 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
-namespace Yupao\Tracing\Hyperf;
+namespace Yupao\Tracing;
 
 use GuzzleHttp\Client;
 use Yupao\Tracing\Hyperf\Listener\DbQueryExecutedListener;
 use Jaeger\ThriftUdpTransport;
+use Yupao\Tracing\Hyperf\SpanTagManager;
+use Yupao\Tracing\Hyperf\SpanTagManagerFactory;
+use Yupao\Tracing\Hyperf\SwitchManager;
+use Yupao\Tracing\Hyperf\SwitchManagerFactory;
+use Yupao\Tracing\Hyperf\TracerFactory;
 use Yupao\Tracing\OpenTracing\Tracer;
 
-/**
- * @deprecated
- */
 class ConfigProvider
 {
     public function __invoke(): array
